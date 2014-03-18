4faadc95bda7040c9da2358172ee5cd5f0a8dbfc
montanaj <clairejencks@gmail.com>
2014-03-18 13:25:30 -0500

    first commit

---
+++ MCMS/CMJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMJAppDelegate.h
+//  MCMS
+//
+//  Created by Claire Jencks on 3/18/14.
+//  Copyright (c) 2014 Claire Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
